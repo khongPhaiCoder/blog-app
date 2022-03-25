d4bbecd9bb1cffda8c1d2969bcb4552a884fcb5a
SahGW <soiamhon@gmail.com>
2022-03-25 07:06:31 +0700

    config mongoose multer morgan

---
+++ api/index.js
@@ -0,0 +1,39 @@
+const path = require("path");
+
+const express = require("express");
+const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const helmet = require("helmet");
+const multer = require("multer");
+const morgan = require("morgan");
+
+const connectDatabase = require("./src/configs/db.config");
+const morganConfig = require("./src/configs/morgan.config");
+const multerConfig = require("./src/configs/multer.config");
+
+dotenv.config();
+connectDatabase();
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+
+app.use(cors());
+app.use(helmet());
+app.use(bodyParser.json());
+app.use(morgan("combined", { stream: morganConfig.accessLogStream }));
+app.use(multer(multerConfig.options).fields(multerConfig.fields));
+app.use(
+    "/images",
+    (req, res, next) => {
+        res.set("Access-Control-Allow-Origin", "*");
+        res.set("Cross-Origin-Resource-Policy", "same-site");
+        next();
+    },
+    express.static(path.join(__dirname, "public", "images"))
+);
+
+app.listen(port, () => {
+    console.log(`Listening on port ${port}`);
+});
---
+++ api/src/configs/multer.config.js
@@ -0,0 +1,36 @@
+const path = require("path");
+
+const multer = require("multer");
+const { v4: uuidV4 } = require("uuid");
+
+const multerConfig = {};
+
+const fileStorage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, "public/images");
+    },
+    filename: (req, file, cb) => {
+        cb(null, uuidV4() + path.extname(file.originalname));
+    },
+});
+
+const fileFilter = (req, file, cb) => {
+    if (
+        file.mimetype === "image/png" ||
+        file.mimetype === "image/jpeg" ||
+        file.mimetype === "image/jpg"
+    ) {
+        cb(null, true);
+    } else {
+        cb(null, false);
+    }
+};
+
+multerConfig.options = {
+    storage: fileStorage,
+    fileFilter: fileFilter,
+};
+
+multerConfig.fields = [{ name: "profilePicture", maxCount: 1 }];
+
+module.exports = multerConfig;
