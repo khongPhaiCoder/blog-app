dbf08aa04c8601231f906cc9dc0d598e25c32d4e
SahGW <soiamhon@gmail.com>
2022-03-27 01:46:14 +0700

    authenticated role route

--- api/src/routes/role.route.js
+++ api/src/routes/role.route.js
@@ -3,9 +3,14 @@ const router = require("express").Router();
 const RoleController = require("../controllers/role.controller");
 const RoleMiddleware = require("../middleware/role.middleware");
 const requestValidation = require("../middleware/request-validation.middleware");
+const authenticationMiddleware = require("../middleware/authentication.middleware");
 
 router
     .route("/")
+    .all(
+        authenticationMiddleware.authenticateUser,
+        authenticationMiddleware.authorizePermissions
+    )
     .get(RoleController.getRoles)
     .post(
         RoleMiddleware.bodyNewRoleValidation,
