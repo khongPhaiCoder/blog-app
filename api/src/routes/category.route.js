6401923d7f99e1599bdc1bc89fa2a5f94a6ccb55
SahGW <soiamhon@gmail.com>
2022-03-27 01:19:45 +0700

    authenticated category route

--- api/src/routes/category.route.js
+++ api/src/routes/category.route.js
@@ -3,9 +3,14 @@ const router = require("express").Router();
 const CategoryController = require("../controllers/category.controller");
 const CategoryMiddleware = require("../middleware/category.middleware");
 const requestValidation = require("../middleware/request-validation.middleware");
+const authenticationMiddleware = require("../middleware/authentication.middleware");
 
 router
     .route("/")
+    .all(
+        authenticationMiddleware.authenticateUser,
+        authenticationMiddleware.authorizePermissions
+    )
     .get(CategoryController.getCategories)
     .post(
         CategoryMiddleware.bodyNewCategoryValidation,
