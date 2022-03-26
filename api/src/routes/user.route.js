5a0b955de0a4b4d98e4aca6dde16e7bdf4275643
SahGW <soiamhon@gmail.com>
2022-03-27 01:47:53 +0700

    authenticated user route

--- api/src/routes/user.route.js
+++ api/src/routes/user.route.js
@@ -3,11 +3,16 @@ const router = require("express").Router();
 const UserController = require("../controllers/user.controller");
 const UserMiddleware = require("../middleware/user.middleware");
 const requestValidation = require("../middleware/request-validation.middleware");
+const authenticationMiddleware = require("../middleware/authentication.middleware");
 
 router
     .route("/:userId")
     .all(UserMiddleware.paramsValidation, requestValidation)
     .get(UserController.getUser)
+    .all(
+        authenticationMiddleware.authenticateUser,
+        authenticationMiddleware.authorizePermissions
+    )
     .put(
         UserMiddleware.bodyUpdateValidation,
         requestValidation,
