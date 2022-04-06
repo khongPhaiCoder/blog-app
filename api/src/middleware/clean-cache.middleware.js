61fb3f85d0d2954118dd82bff4ace01d41317718
SahGW <soiamhon@gmail.com>
2022-04-06 10:39:12 +0700

    fix clean-cache

--- api/src/middleware/clean-cache.middleware.js
+++ api/src/middleware/clean-cache.middleware.js
@@ -1,10 +1,11 @@
 const { clearHash } = require("../services/cache.service");
+const wrapAsync = require("../utils/wrap-async");
 
-module.exports = async (req, res, next) => {
+module.exports = wrapAsync(async (req, res, next) => {
     await next();
 
     if (req.userId) {
         clearHash(req.userId);
     }
     clearHash(process.env.GUEST_ID);
-};
+});
